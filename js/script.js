2e5240cf749cc54420b7fe6c8ef81daf7016c34e
xClune <aclune382@gmail.com>
2024-03-01 14:09:34 +0800

    Grid generation to function.
    Create button for generating new grid of custom size from user prompt.
    
    Add 'selected' class onclick of cell div.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
-
-
 // create 16x16 grid of divs
 const grid = document.querySelector('.grid-container');
 
-for (let i = 0; i < (16); i++){
-    const row = document.createElement('div');
-    row.style.display = 'flex';
-    row.style.width = '100%';
-    row.classList.add(`row-${i}`);
-    grid.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-    const cell = document.createElement('div');
-    cell.style.border = '1px solid black';
-    cell.style.flex = 1;
-    cell.style.height = '30px';
-    cell.classList.add(`cell-${(i+1)}-${(j+1)}`);
+createGrid();
 
-    cell.addEventListener("mouseover", () => {
-        cell.classList.add('hovering');
-    })
-    cell.addEventListener("mouseout", () => {
-        cell.classList.remove('hovering');
-    })
+// grid size button
+let gridButton = document.querySelector('.size-btn');
+gridButton.addEventListener('click', () => {
+    let size = prompt("Enter desired grid size", "Size (e.g. '16')");
+    size;
+    grid.innerHTML = '';
+    createGrid(size);
+})
 
-    row.appendChild(cell);
-    }   
+function createGrid(size = 16) {
+    for (let i = 0; i < (size); i++){
+        const row = document.createElement('div');
+        row.style.display = 'flex';
+        row.style.width = '100%';
+        row.classList.add(`row-${i}`);
+        grid.appendChild(row);
+        for (let j = 0; j < size; j++) {
+        const cell = document.createElement('div');
+        cell.style.border = '1px solid black';
+        cell.style.flex = 1;
+        cell.style.height = `${400/size}px`;
+        cell.classList.add(`cell-${(i+1)}-${(j+1)}`);
+    
+        cell.addEventListener("mouseover", () => {
+            cell.classList.add('hovering');
+        })
+        cell.addEventListener("mouseout", () => {
+            cell.classList.remove('hovering');
+        })
+        cell.addEventListener("click", () => {
+            cell.classList.add('selected');
+        })
+        row.appendChild(cell);
+        }   
+    }
 }
